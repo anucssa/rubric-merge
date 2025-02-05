cb9b1e2518362e283db8ac96f522082b2b6d1316
trickypr <git@trickypr.com>
2025-02-05 23:26:16 +1100

    fix: add package build deps

--- nix/package.nix
+++ nix/package.nix
@@ -1,6 +1,9 @@
-{ craneLib, ... }:
+{ craneLib, pkgs, ... }:
 craneLib.buildPackage rec {
   pname = "rubric-merge";
   name = pname;
   src = craneLib.cleanCargoSource ../.;
+
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  buildInputs = with pkgs; [ openssl ];
 }
