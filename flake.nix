f34dc1e147a8c93d7397ed73970862c669f04350
trickypr <git@trickypr.com>
2025-02-05 17:58:18 +1100

    chore: setup project structure

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "merge rubric and postgres membership databases";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              rust-bin.stable.latest.default
+            ];
+
+            shellHook = '''';
+          };
+      }
+    );
+}
